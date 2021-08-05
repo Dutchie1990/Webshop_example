4286e5f5448a1a850b1bd6225ae5bdd28520dbd5
Dutchie1990 <danielboone1990@gmail.com>
2021-08-05 11:12:58 +0000

    setting up toasters

--- webshop_example/settings.py
+++ webshop_example/settings.py
@@ -162,6 +162,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
